3d5705461731c3a6fdc0c9d1d73f55565071efab
Markus Unterwaditzer <markus-tarpit+git@unterwaditzer.net>
2023-06-24 15:14:41 +0200

    fix #13 -- global undo stack

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod expr;
 
 use std::collections::VecDeque;
 use std::fs;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use anyhow::{Context, Error};
 use console::{style, Key};
@@ -85,6 +85,8 @@ fn main() -> Result<(), Error> {
         .context("failed to parse search string")?;
     let replacer = expr.get_replacer(multiline, user_defined_pairs)?;
 
+    let mut undo_stack = VecDeque::new();
+
     let mut walk_builder = if file_or_dir.is_empty() {
         ignore::WalkBuilder::new(".")
     } else {
@@ -106,31 +108,41 @@ fn main() -> Result<(), Error> {
         walk_builder.overrides(builder.build()?);
     }
 
-    'files: for entry in walk_builder.build() {
-        let entry = entry?;
-        let filetype = entry.file_type().unwrap();
-        if !filetype.is_file() {
-            continue;
-        }
-
-        let mut file = match fs::read_to_string(entry.path()) {
+    let walker = walk_builder.build().filter_map(|entry| {
+        let entry = match entry {
             Ok(x) => x,
-            Err(_) => continue, // presumably binary file
+            Err(e) => return Some(Err(e)),
         };
 
-        let mut old_files = VecDeque::new();
+        let filetype = entry.file_type().unwrap();
+        if !filetype.is_file() {
+            return None;
+        }
+        let file_path = entry.path().to_owned();
+        let file = match fs::read_to_string(&file_path) {
+            Ok(x) => x,
+            Err(_) => return None, // presumably binary file
+        };
+
+        Some(Ok((file_path, file)))
+    });
+    let mut walker = itertools::put_back_n(walker);
+
+    'files: while let Some(result) = walker.next() {
+        let (mut file_path, mut file) = result?;
+
         let mut flashed_message = None;
 
         let mut changed = false;
 
         let mut change_i = 0;
 
-        let change_file = |file: &mut String, to: String| -> Result<(), Error> {
-            if fs::read_to_string(entry.path()).map_or(true, |x| x != *file) {
+        let change_file = |file_path: &Path, file: &mut String, to: String| -> Result<(), Error> {
+            if fs::read_to_string(file_path).map_or(true, |x| x != *file) {
                 anyhow::bail!("file was changed while spacemod is running. Bailing out!");
             }
             *file = to;
-            fs::write(entry.path(), &*file)?;
+            fs::write(file_path, &*file)?;
             Ok(())
         };
 
@@ -148,7 +160,7 @@ fn main() -> Result<(), Error> {
                 print_changeset(changeset);
                 println!(
                     "\n\n\n{path} [{change_i}] {flashed_message}",
-                    path = style(entry.path().display()).blue(),
+                    path = style(file_path.display()).blue(),
                     change_i = style(change_i).yellow(),
                     flashed_message = style(flashed_message.take().unwrap_or_default()).red()
                 );
@@ -178,8 +190,15 @@ fn main() -> Result<(), Error> {
                         accept_all = true;
                     }
                     PromptAnswer::Undo => {
-                        if let Some(old_file) = old_files.pop_front() {
-                            change_file(&mut file, old_file)?;
+                        if let Some((old_file_path, change_from, change_to)) =
+                            undo_stack.pop_front()
+                        {
+                            if old_file_path != file_path {
+                                walker.put_back(Ok((file_path, file.clone())));
+                            }
+                            file_path = old_file_path;
+                            file = change_from;
+                            change_file(&file_path, &mut file, change_to)?;
                             change_i -= 1;
                             continue;
                         } else {
@@ -193,16 +212,16 @@ fn main() -> Result<(), Error> {
 
             changed = true;
 
-            old_files.push_front(file.to_string());
-            old_files.truncate(1024);
+            undo_stack.push_front((file_path.clone(), new_file.to_string(), file.to_string()));
+            undo_stack.truncate(1024);
 
             let new_file = new_file.to_string();
-            change_file(&mut file, new_file)?;
+            change_file(&file_path, &mut file, new_file)?;
             change_i += 1;
         }
 
         if accept_all && changed {
-            println!("changed {}", entry.path().display());
+            println!("changed {}", file_path.display());
         }
     }
 
